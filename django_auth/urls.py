7a6e6b4b299dd8ffb6c397c6c68f04212e6f11f4
angela80 <angeedowling@yahoo.com>
2019-02-18 22:12:57 +0000

    initial commit

---
+++ django_auth/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from accounts.views import index
+from accounts import urls as accounts_urls
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name="index"),
+    url(r'^accounts/', include(accounts_urls))
+]
